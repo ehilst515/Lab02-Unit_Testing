0b785137e19e1d8584543ecbe9f9d0c2d9334cb2
Enrique Hilst <enrique.hilst@gmail.com>
2020-08-25 16:40:03 -0500

    initial setup

---
+++ UnitTesting/ViewBalance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace UnitTesting
+{
+    public class ViewBalance
+    {
+        public static decimal ViewBalance()
+        {
+            int balance = 100;
+        }
+    }
+}
+    
